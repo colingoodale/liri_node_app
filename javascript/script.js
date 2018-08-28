043598fad5a381a535c830e0d9f0177584bca06b
Colin <colingoodale@gmail.com>
2018-08-28 16:27:33 -0500

    Ajax on click through function, OMDB created. To be done next is Spotify and Bands in Town.

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,18 @@
-
-movieArt = $("#movieInput").val();
-
-
-
-
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+    omdbSearch();
+})
 
 
+function omdbSearch() {
+    var title = $("#input").val();
+    var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
 
-
-$("#artistConcert").on(function (event) {
-    var concArt = $("#concertInput").val().trim();
-    $.ajax('https://api.bandsintown.com/artists/' + concArt + '/events.json', {
-        data: {
-            api_version: '2.0',
-            app_id: 'eyJUb2tlblR5cGUiOiJBUEkiLCJzYWx0IjoiY2ZhZGZhMzUtOTJiMC00NWUzLWExNmItNzA0MWY0ZTUzZWY2IiwiYWxnIjoiSFM1MTIifQ.eyJqdGkiOiIxMWRiZWQ0Zi05ZmU3LTQ1ZDEtYTY3YS04YmZlZTEyY2NjZDkiLCJpYXQiOjE1MzU0MDY4MDR9.Kd4JR1d1rXksIq5XGFsG1Yk6sTyzn106FqnN8RPXZLJkNoGFVzXTOpOBmJQ6l52AljPC0o1222O-8fDt1JOuww'
-        },
-        dataType: 'jsonp',
-        jsonpCallback: 'response',
-        crossDomain: true
-    })
-
-    console.log(response)
-
-});
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
+}
+omdbSearch();
